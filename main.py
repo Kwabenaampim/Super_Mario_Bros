24a2793603aa3de0cd7770ea64bdc5d0d0e8c298
HeHanyu1 <87448530+HeHanyu1@users.noreply.github.com>
2021-12-14 17:47:40 +0100

    Minor changes
    asd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyxel
+import constant
 from board import Board
 board = Board(256, 256)
-pyxel.init(board.width,board.height, caption = 'Drawing test', fps = 60)
+pyxel.init(board.width,board.height, caption = constant.CAPTION, fps = 60)
 pyxel.load('assets/marioassets.pyxres')
 pyxel.run(board.update, board.draw)

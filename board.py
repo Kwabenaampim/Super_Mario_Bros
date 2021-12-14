a1a6e66f13c6cde4db5d8a3be517a149a46d87db
HeHanyu1 <87448530+HeHanyu1@users.noreply.github.com>
2021-12-14 20:53:30 +0100

    coomments
    hh

--- board.py
+++ board.py
@@ -10,21 +10,15 @@ from goomba import Goomba
 from koopa_troopa import Koopa
 from bushes import Bushes
 from clouds import Clouds
-
-# This is another library neccessary to create certain random elements
-from random import randint
-# The following imports have to be decompossed in the multiple classes
-#from floordrawing import Koopa, Goomba,Bushes,Clouds, PLayer, Floor, BreakableBrickBlock, Pipes, QuestionBlock, Scoreboard, ClearBlock, BrickBlockWithCoins
-# Here later we import the constants
+# This imports the constants
 import constant
 
 # here starts the board class
 class Board:
     def __init__(self, w, h):
+        # This will give the height and width of the board
         self.width = w
         self.height = h
-        # This keeps track of the enemies on screen but don't know how to do so
-        self.__enemies_list = [randint(0,3), randint(0,3), randint(0,3), randint(0,3)]
         
         # From here most of the classes are initialized, which are neccessary to make most of drawings
         # Player
@@ -53,7 +47,7 @@ class Board:
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
-        # this is responsible for the control of players
+        # this is responsible for the control of Mario
         if pyxel.btnp(pyxel.KEY_RIGHT, hold=True, period=1):
             self.mario.move('right', self.width)
         if pyxel.btnp(pyxel.KEY_LEFT, hold=True, period=1):
@@ -71,6 +65,7 @@ class Board:
         # The goomba is the hard one, it needs to check collisions with
         # All of the elements in the environment -> not do this part
         self.goomba.goomba_move()
+        # Here the update method of the different blocks where to be added
         
     def draw(self):
         pyxel.cls(12)
@@ -87,13 +82,17 @@ class Board:
         pyxel.text(0, 0, 'Scoreboard', 7)
         pyxel.text(0, 10,str(self.scoreboard.score), 7)
         # Coins
-        pyxel.blt(100, 9, constant.COIN_IMAGE[0], constant.COIN_IMAGE[1], constant.COIN_IMAGE[2], constant.COIN_IMAGE[3],constant.COIN_IMAGE[4] ) # To be substituted with the coin image
+        # Coin image
+        pyxel.blt(100, 9, constant.COIN_IMAGE[0], constant.COIN_IMAGE[1], constant.COIN_IMAGE[2], constant.COIN_IMAGE[3],constant.COIN_IMAGE[4] )
         pyxel.text(110, 10, 'X'+str(self.scoreboard.coin_amount), 7)
         
         # The following draws the elements across the 4 screens 
         for i in range(0, 1025, 16): 
             # Decorations of the level
             # Clouds
+            # Functionality: most of the conditionals will draw the corresponding sprite if it's found in the list predefined, such list has the x coordinates
+            # Mostly are defined off-screen. If not the sprite is not drawn, pretty straightforward. The images are moved with respect to the 'decrease' variable
+            # of Mario and thus makes the level have the illusion of moving through the level.
             if i in self.clouds.coordinates:
                 pyxel.blt(i -self.mario.decrease, self.clouds.y, constant.CLOUD_IMAGE[0], constant.CLOUD_IMAGE[1], constant.CLOUD_IMAGE[2], constant.CLOUD_IMAGE[3], constant.CLOUD_IMAGE[4])
             # Bushes
@@ -121,10 +120,12 @@ class Board:
                 if i == self.pipes.pipes_list[1]:
                     # The for loop inside ensures the bottom pipe is drawn before the 'top pipe'
                     # So that it gives the 1 single pipe drawing
+                    # The inner for loop ensures that the lower pipe is drawn before the top pipe, so to make the pipe look like a continuous one
                     for o in range(0,2):
                         pyxel.blt(i-self.mario.decrease, self.pipes.y+(16-(o*16)), constant.PIPE_IMAGE[0], constant.PIPE_IMAGE[1], constant.PIPE_IMAGE[2], constant.PIPE_IMAGE[3], constant.PIPE_IMAGE[4])
-            # The enemy generator conditional
-            # While there aren't enemies on the screen keep generating enemies every 4 seconds
+            # The enemy drawings
+            # Since we couldn't think of a conditional that made enemies appear every x seconds we decided to go for 'predefined randomness'
+            # That is we generate them throughout the level in an order 
             # This generates a goomba
             if i in self.goomba.goomba_position:
                 pyxel.blt(i-self.mario.decrease + self.goomba.goomba_x ,  self.goomba.goomba_y, constant.GOOMBA_IMAGE[0] , constant.GOOMBA_IMAGE[1], constant.GOOMBA_IMAGE[2], constant.GOOMBA_IMAGE[3], constant.GOOMBA_IMAGE[4], 12)
